1de6ec4e35236147e0ee01a88c3af6a2d8402fff
Vera Gonzalez <organizedgrime.svg@gmail.com>
2023-02-07 20:35:03 -0500

    Updated Familiar Spirit Rive, hooking into its state machine with React to move eyes

--- src/components/rives/FamiliarSpirit.jsx
+++ src/components/rives/FamiliarSpirit.jsx
@@ -1,20 +1,50 @@
+import { useEffect, useState } from 'react';
 import { 
-  useRive, 
+  useRive,
   useStateMachineInput,
-  Layout, 
-  Fit, 
-  Alignment 
+  Layout,
+  Fit,
+  Alignment
 } from '@rive-app/react-canvas';
 
 export default function FamiliarSpirit() {
   const { rive, RiveComponent } = useRive({
     src: '/rives/familiar-spirit.riv',
-    autoplay: false,
+    autoplay: true,
+    stateMachines: "LookAround",
     layout: new Layout({
       fit: Fit.Fill,
       alignment: Alignment.TopCenter,
     }),
   });
+
+  const [maxWidth, setMaxWidth] = useState();
+  const [maxHeight, setMaxHeight] = useState();
+
+  const xAxisInput = useStateMachineInput(rive, "LookAround", "xAxis", 0);
+  const yAxisInput = useStateMachineInput(rive, "LookAround", "yAxis", 0);
+
+  useEffect(() => {
+    const body = document.querySelector("main");
+    if (body) {
+      const bodyRect = body.getBoundingClientRect();
+      setMaxWidth(bodyRect.right);
+      setMaxHeight(bodyRect.bottom);
+    }
+  }, []);
+
+  useEffect(() => {
+    const update = (e) => {
+      if (maxWidth && maxHeight && yAxisInput && xAxisInput) {
+        xAxisInput.value = (e.x / maxWidth) * 100;
+        yAxisInput.value = (e.y / maxHeight) * 100;
+      }
+    };
+    window.addEventListener("mousemove", update);
+    return () => {
+      window.removeEventListener("mousemove", update);
+    };
+  }, [xAxisInput, yAxisInput, maxHeight, maxWidth]);
 
   return <RiveComponent />;
 }
