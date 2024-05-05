9d71d287884cf10f3b997b2bbc2113f4b1569f35
Vera Gonzalez <me@vera.lgbt>
2024-05-05 17:52:27 -0400

    astro nano boiler

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+	],
+};
