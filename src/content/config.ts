1ddbed60bd9c5f2effd37a469f092e97318b0b8b
Vera Gonzalez <me@vera.lgbt>
2024-05-05 20:16:23 -0400

    added poems

--- src/content/config.ts
+++ src/content/config.ts
@@ -32,4 +32,16 @@ const creations = defineCollection({
     }),
 });
 
+const poems = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.coerce.date(),
+        draft: z.boolean().optional(),
+        demoURL: z.string().optional(),
+        repoURL: z.string().optional()
+    }),
+});
+
 export const collections = { blog, work, creations };
