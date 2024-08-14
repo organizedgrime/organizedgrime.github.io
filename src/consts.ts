e364d818989c008a22367f3afa9ac6ab7dd44d43
Vera Gonzalez <me@vera.lgbt>
2024-08-14 17:11:06 -0400

    updated rss

--- src/consts.ts
+++ src/consts.ts
@@ -1,56 +1,56 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-    NAME: "vera.lgbt",
-    EMAIL: "me@vera.lgbt",
-    NUM_POSTS_ON_HOMEPAGE: 3,
-    NUM_WORKS_ON_HOMEPAGE: 2,
-    NUM_CREATIONS_ON_HOMEPAGE: 3,
+	NAME: "vera.lgbt",
+	EMAIL: "me@vera.lgbt",
+	NUM_POSTS_ON_HOMEPAGE: 3,
+	NUM_WORKS_ON_HOMEPAGE: 2,
+	NUM_CREATIONS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-    TITLE: "Home",
-    DESCRIPTION: "I'm Vera; this website showcases me and my work.",
+	TITLE: "Veraverse",
+	DESCRIPTION: "I'm Vera; this website showcases me and my work.",
 };
 
 export const WORK: Metadata = {
-    TITLE: "Work",
-    DESCRIPTION: "Where I have worked and what I have done.",
+	TITLE: "Work",
+	DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const CREATIONS: Metadata = {
-    TITLE: "Creations",
-    DESCRIPTION: "A collection of my creations, with links to repositories and demos.",
+	TITLE: "Creations",
+	DESCRIPTION: "A collection of my creations, with links to repositories and demos.",
 };
 
 export const SOFTWARE: Metadata = {
-    TITLE: "Software",
-    DESCRIPTION: "Dirty talking with silicon.",
+	TITLE: "Software",
+	DESCRIPTION: "Dirty talking with silicon.",
 };
 
 export const LINGUISTICS: Metadata = {
-    TITLE: "Linguistics",
-    DESCRIPTION: "Collection of language study.",
+	TITLE: "Linguistics",
+	DESCRIPTION: "Collection of language study.",
 };
 
 export const MATHS: Metadata = {
-    TITLE: "Math & Science",
-    DESCRIPTION: "Sometimes I think really hard.",
+	TITLE: "Math & Science",
+	DESCRIPTION: "Sometimes I think really hard.",
 }
 
 export const POEMS: Metadata = {
-    TITLE: "Poem",
-    DESCRIPTION: "At some point I started writing poetry.<br/> I even like some of it.",
+	TITLE: "Poem",
+	DESCRIPTION: "At some point I started writing poetry.<br/> I even like some of it.",
 };
 
 
 export const SOCIALS: Socials = [
-    {
-        NAME: "github",
-        HREF: "https://github.com/organizedgrime"
-    },
-    {
-        NAME: "linkedin",
-        HREF: "https://www.linkedin.com/in/vera-gonzalez",
-    }
+	{
+		NAME: "github",
+		HREF: "https://github.com/organizedgrime"
+	},
+	{
+		NAME: "linkedin",
+		HREF: "https://www.linkedin.com/in/vera-gonzalez",
+	}
 ];
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,25 +3,52 @@ import { getCollection } from "astro:content";
 import { HOME } from "@consts";
 
 type Context = {
-    site: string
+	site: string
 }
 
+
 export async function GET(context: Context) {
-    const creations = (await getCollection("creations"))
-        .filter(creation => !creation.data.draft);
+	function draft(item) {
+		return !item.data.draft;
+	}
 
-    const items = creations
-        .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+	function entry(item) {
+		return {
+			title: item.data.title,
+			description: item.data.description,
+			pubDate: item.data.date,
+			link: `/${item.collection}/${item.slug}/`,
+		};
+	}
 
-    return rss({
-        title: HOME.TITLE,
-        description: HOME.DESCRIPTION,
-        site: context.site,
-        items: items.map((item) => ({
-            title: item.data.title,
-            description: item.data.description,
-            pubDate: item.data.date,
-            link: `/${item.collection}/${item.slug}/`,
-        })),
-    });
+	const creations = (await getCollection("creations"))
+		.filter(draft)
+		.map(entry);
+
+	const poems = (await getCollection("poems"))
+		.filter(draft)
+		.map(entry);
+
+	const math = (await getCollection("math"))
+		.filter(draft)
+		.map(entry);
+
+	const linguistics = (await getCollection("linguistics"))
+		.filter(draft)
+		.map(entry);
+
+	const software = (await getCollection("software"))
+		.filter(draft)
+		.map(entry);
+
+	const items = [creations, poems, math, linguistics, software]
+		.flat()
+		.sort((a, b) => new Date(b.pubDate).valueOf() - new Date(a.pubDate).valueOf());
+
+	return rss({
+		title: HOME.TITLE,
+		description: HOME.DESCRIPTION,
+		site: context.site,
+		items: items,
+	});
 }
