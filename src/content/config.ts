b1d28e71f7309f68f7a10ecad91009546293d038
Vera Gonzalez <me@vera.lgbt>
2024-08-14 17:19:48 -0400

    fmt

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,67 +1,67 @@
 import { defineCollection, z } from "astro:content";
 
 const work = defineCollection({
-    type: "content",
-    schema: z.object({
-        company: z.string(),
-        role: z.string(),
-        dateStart: z.coerce.date(),
-        dateEnd: z.union([z.coerce.date(), z.string()]),
-        draft: z.boolean().optional(),
-    }),
+	type: "content",
+	schema: z.object({
+		company: z.string(),
+		role: z.string(),
+		dateStart: z.coerce.date(),
+		dateEnd: z.union([z.coerce.date(), z.string()]),
+		draft: z.boolean().optional(),
+	}),
 });
 
 const creations = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        draft: z.boolean().optional(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 const software = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        draft: z.boolean().optional(),
-        demoURL: z.string().optional(),
-        repoURL: z.string().optional()
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+		demoURL: z.string().optional(),
+		repoURL: z.string().optional()
+	}),
 });
 
 
 const math = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        draft: z.boolean().optional(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 const linguistics = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        draft: z.boolean().optional(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 const poems = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        date: z.coerce.date(),
-        draft: z.boolean().optional(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string().optional(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 export const collections = { work, creations, poems, math, software, linguistics };
