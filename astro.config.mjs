a65becb2f2b1782c4d8cd0b8b87d2e80ce849526
Vera Gonzalez <me@vera.lgbt>
2025-10-06 16:56:11 -0400

    tina init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,40 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import { KatexOptions } from 'katex';
+import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vera.lgbt",
-	integrations: [mdx(), sitemap(), tailwind()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [
-			[rehypeKatex, {
-				macros: {
-					"\\RR": "\\mathbb{R}",
-					"\\ZZ": "\\mathbb{Z}",
-					"\\CC": "\\mathbb{C}",
-					"\\QQ": "\\mathbb{Q}",
-					"\\NN": "\\mathbb{N}",
-					"\\FF": "\\mathbb{F}",
-					"\\FF": "\\mathbb{F}",
-					"\\B": "\\boldsymbol{#1}",
-					"\\b": "\\mathbf{#1}",
-					"\\id": "\\htmlId{eq:#1}{#2}",
-					"\\t": "\\htmlId{eq:#1}{#2} \\tag{#1}",
-					"\\while": "\\rm{\\bold{while}} \\space #1 \\space \\rm{\\bold{do}}",
-					"\\algorithm": "\\hline \\rm{\\bold{Algorithm\\>#1}\\>#2}\\\\ \\hline "
-				},
-				trust: true,
-				leqno: true,
-			}]
-		]
-	}
+  site: "https://vera.lgbt",
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [
+        rehypeKatex,
+        {
+          macros: {
+            "\\RR": "\\mathbb{R}",
+            "\\ZZ": "\\mathbb{Z}",
+            "\\CC": "\\mathbb{C}",
+            "\\QQ": "\\mathbb{Q}",
+            "\\NN": "\\mathbb{N}",
+            "\\FF": "\\mathbb{F}",
+            "\\B": "\\boldsymbol{#1}",
+            "\\b": "\\mathbf{#1}",
+            "\\id": "\\htmlId{eq:#1}{#2}",
+            "\\t": "\\htmlId{eq:#1}{#2} \\tag{#1}",
+            "\\while":
+              "\\rm{\\bold{while}} \\space #1 \\space \\rm{\\bold{do}}",
+            "\\algorithm":
+              "\\hline \\rm{\\bold{Algorithm\\>#1}\\>#2}\\\\ \\hline ",
+          },
+          trust: true,
+          leqno: true,
+        },
+      ],
+    ],
+  },
 });
