eb5a5bb919e285f7e9b90f2903804b5ad822f1b2
Vera Gonzalez <me@vera.lgbt>
2025-10-07 01:12:32 -0400

    runtime

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,30 +1,33 @@
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
 import { loadEnv } from "vite";
+
 const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
   PUBLIC_SANITY_STUDIO_DATASET,
   PUBLIC_SANITY_PROJECT_ID,
   PUBLIC_SANITY_DATASET,
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
 import { defineConfig } from "astro/config";
 
 // Different environments use different variables
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
-import sanity from "@sanity/astro";
 import react from "@astrojs/react";
-
 // Change this depending on your hosting provider (Vercel, Netlify etc)
 // https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
 import vercel from "@astrojs/vercel/serverless";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   // Hybrid+adapter is required to support embedded Sanity Studio
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    runtime: "nodejs20.x",
+  }),
   integrations: [
     sanity({
       projectId,
@@ -32,7 +35,7 @@ export default defineConfig({
       // studioBasePath: "/admin",
       useCdn: false,
       // `false` if you want to ensure fresh data
-      apiVersion: "2024-12-08", // Set to date of setup to use the latest API version
+      apiVersion: "2025-10-05", // Set to date of setup to use the latest API version
     }),
     react(), // Required for Sanity Studio
   ],
