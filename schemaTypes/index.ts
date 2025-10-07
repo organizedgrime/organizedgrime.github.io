f5632178ca14009cb0417ae19fa2b47d1a522092
Vera Gonzalez <me@vera.lgbt>
2025-10-07 00:21:31 -0400

    testing

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import author from "./author";
+import blockContent from "./blockContent";
+import category from "./category";
+import poem from "./poem";
+import post from "./post";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, poem, author, category, blockContent];
