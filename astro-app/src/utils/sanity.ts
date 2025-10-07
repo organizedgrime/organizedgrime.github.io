09f572c2a15185391f923cab4e981944fea40021
Vera Gonzalez <me@vera.lgbt>
2025-10-07 06:22:17 -0400

    astro

--- astro-app/src/utils/sanity.ts
+++ astro-app/src/utils/sanity.ts
@@ -3,20 +3,34 @@ import type { PortableTextBlock } from "@portabletext/types";
 import type { ImageAsset, Slug } from "@sanity/types";
 import groq from "groq";
 
-export async function getPosts(): Promise<Post[]> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`
-  );
+type DocumentType = "post" | "origami" | "article" | "poem";
+
+export async function getPosts<T>(type?: DocumentType): Promise<T[]> {
+  if (type) {
+    return await sanityClient.fetch(
+      groq`*[_type == "${type}" && defined(slug.current)] | order(_createdAt desc)`,
+    );
+  } else {
+    return await sanityClient.fetch(
+      groq`*[defined(slug.current)] | order(_createdAt desc)`,
+    );
+  }
 }
 
-export async function getPost(slug: string): Promise<Post> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && slug.current == $slug][0]`,
-    {
-      slug,
-    }
-  );
+export async function getPost<T>(
+  slug: string,
+  type?: DocumentType,
+): Promise<T> {
+  if (type) {
+    return await sanityClient.fetch(
+      groq`*[_type == "${type}" && slug.current == ${slug}][0]`,
+    );
+  } else {
+    return await sanityClient.fetch(groq`*[slug.current == ${slug}][0]`);
+  }
 }
+
+export type Photo = ImageAsset & { alt?: string };
 
 export interface Post {
   _type: "post";
@@ -24,6 +38,16 @@ export interface Post {
   title?: string;
   slug: Slug;
   excerpt?: string;
-  mainImage?: ImageAsset & { alt?: string };
+  photo?: Photo;
+  body: PortableTextBlock[];
+}
+
+export interface Origami {
+  _type: "post";
+  _createdAt: string;
+  title?: string;
+  slug: Slug;
+  date?: Date;
+  gallery?: Photo[];
   body: PortableTextBlock[];
 }
