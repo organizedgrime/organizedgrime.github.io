f0b88518a227f22dc265c31426b96e85d39fac47
Vera Gonzalez <me@vera.lgbt>
2025-11-24 12:06:46 -0500

    fix preview migration

--- migrations/preview.ts
+++ migrations/preview.ts
@@ -3,7 +3,7 @@ export default defineMigration({
   title: "Default `preview` to `false` for all gallery items",
   documentTypes: ["post"],
   filter:
-    "defined(gallery) && count(gallery[]._ref) != count(gallery[].preview)",
+    "defined(gallery) && count(gallery[]._ref) != count(gallery[].preview != undefined)",
   migrate: {
     document(post) {
       const gallery = Array.isArray(post.gallery) ? post.gallery : [];
